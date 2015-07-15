86a315ea856d60933d6635eb71c7e01cae3bb606
Clay Brehm <clay.brehm@gmail.com>
2015-07-15 17:50:59 -0400

    added model and interface

--- app/src/main/java/org/coursera/claybrehm/kewlvideo/model/VideoMetadata.java
+++ app/src/main/java/org/coursera/claybrehm/kewlvideo/model/VideoMetadata.java
@@ -4,4 +4,63 @@ package org.coursera.claybrehm.kewlvideo.model;
  * Created by claybrehm on 7/14/15.
  */
 public class VideoMetadata {
+
+    private String id;
+    private String title;
+    private String author;
+    private int numRates;
+    private long totalStars;
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumRates() {
+        return numRates;
+    }
+
+    public void setNumRates(int numRates) {
+        this.numRates = numRates;
+    }
+
+    public long getTotalStars() {
+        return totalStars;
+    }
+
+    public void setTotalStars(long totalStars) {
+        this.totalStars = totalStars;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public float getRating() {
+        return ((float)totalStars/(float)numRates);
+    }
 }
--- app/src/main/java/org/coursera/claybrehm/kewlvideo/model/VideoService.java
+++ app/src/main/java/org/coursera/claybrehm/kewlvideo/model/VideoService.java
@@ -1,7 +1,37 @@
 package org.coursera.claybrehm.kewlvideo.model;
 
+
+import android.net.Uri;
+
+import java.util.Collection;
+
+import retrofit.client.Response;
+import retrofit.http.GET;
+import retrofit.http.Multipart;
+import retrofit.http.POST;
+import retrofit.http.Part;
+import retrofit.http.Path;
+import retrofit.http.Streaming;
+import retrofit.mime.TypedFile;
+
 /**
  * Created by claybrehm on 7/14/15.
  */
 public interface VideoService {
+
+    @Multipart
+    @POST("/upload")
+    public Uri addVideo(@Part("video") VideoMetadata videoMetadata, @Part("data") TypedFile videoContent);
+
+    @Streaming
+    @GET("/video/{id}")
+    public Response getData(@Path("id") long id);
+
+    @GET("/video")
+    public Collection<VideoMetadata> getVideoList();
+
+    @POST("/video/{id}/stars/{stars}")
+    public VideoMetadata rateVideo(@Path("id") long id, @Path("stars") int stars);
+
+
 }
